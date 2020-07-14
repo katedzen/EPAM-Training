768e828904a0f8f2b991cf8b4c14978607396746
Екатерина Петухова <katedzen@icloud.com>
2020-07-14 14:22:21 +0300

    TryTest

---
+++ PetukhovaTask1.Tests/NodEvklidTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PetukhovaTask1.Tests
+{
+    [TestClass]
+    public class NodEvklidTests
+    {
+        [TestMethod]
+        public void EvklidAlg_10and20_10returned()
+        {
+            int arg1 = 10;
+            int arg2 = 20;
+            int expected = 10;
+
+            
+
+        }
+    }
+}
---
+++ PetukhovaTask1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PetukhovaTask1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PetukhovaTask1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93fdeaf5-0195-431e-beb1-30eab6dd5d0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

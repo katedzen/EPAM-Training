9b1b5fba129f4bcd8676b6921edfc30ab1789d60
katedzen <60621263+katedzen@users.noreply.github.com>
2020-07-19 14:46:46 +0300

    1_2

---
+++ PetukhovaTask1_2/Figure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetukhovaTask1_2
+{
+    ///<summary>
+    ///Базовый класс фигура.
+    ///</summary>
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Название фигуры
+        /// </summary>
+        public string Name { get; protected set; }
+
+        /// <summary>
+        /// Создать новый экземпляр фигуры
+        /// </summary>
+        /// <param name="name"></param>
+        public Figure (string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            Name = name;
+        }
+
+        /// <summary>
+        /// Метод для нахождения периметра фигуры
+        /// </summary>
+        /// <returns>Периметр фигуры</returns>
+        public abstract double Perimeter();
+
+        /// <summary>
+        /// Метод для нахождения площади фигуры
+        /// </summary>
+        /// <returns>Площадь фигуры</returns>
+        public abstract double Area();
+    }
+}

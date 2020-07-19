8af47ab3b9f51323ee1bfdfdbc3c2a8feee78df5
katedzen <60621263+katedzen@users.noreply.github.com>
2020-07-19 15:36:46 +0300

    task1

---
+++ PetukhovaTask1_2/Circle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetukhovaTask1_2
+{
+    /// <summary>
+    /// Тип окружность.
+    /// </summary>
+    class Circle : Figure
+    {
+        public double CentrX { get; set; }
+        public double CentrY { get; set; }
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// Создание окружности по координатам центра и радиусу.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="r"></param>
+        public Circle(string name, double x1, double y1, double r) : base(name) 
+        {
+            CentrX = x1;
+            CentrY = y1;
+            Radius = r;
+        }
+
+        /// <summary>
+        /// Создание окружности по координатам центра и координатам точки на окружности.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        public Circle (string name, double x1, double y1, double x2, double y2) : base(name) 
+        {
+            CentrX = x1;
+            CentrY = x2;
+            Radius = GetDistance(x1, y1, x2, y2);
+        }
+
+        /// <summary>
+        /// Расчет длины окружности.
+        /// </summary>
+        /// <returns>Длина окружности.</returns>
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        /// <summary>
+        /// Расчет площади окружности.
+        /// </summary>
+        /// <returns>Площадь окружности.</returns>
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        /// <summary>
+        /// Получает строковое представление окружности.
+        /// </summary>
+        /// <returns>Строковое представление окружности.</returns>
+        public override string ToString()
+        {
+            return $"{this.Name} - (O({this.CentrX}, {this.CentrY}), {this.Radius})";
+        }
+
+        /// <summary>
+        /// Если передаваемый объект типа Окружность,
+        /// то сравниваются их радиусы. В случае, если они равны, то равны и фигуры.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Если окружности равны - true,
+        /// во всех остальных случаях - false</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Circle circle2)
+            {
+                if (this.Radius == circle2.Radius)
+                    return true;
+            }
+            else return false;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Возвращает некоторое числовое значение,
+        /// соответствующее площади данного объекта.
+        /// </summary>
+        /// <returns>Хэш-код для значения площади фигуры.</returns>
+        public override int GetHashCode()
+        {
+            return this.Area().GetHashCode();
+        }
+    }
+}
--- PetukhovaTask1_2/Quadrangle.cs
+++ PetukhovaTask1_2/Quadrangle.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace PetukhovaTask1_2
 {
     /// <summary>
-    /// Тип четырёхугольник
+    /// Тип четырёхугольник.
     /// </summary>
     class Quadrangle : Figure
     {
@@ -53,6 +53,67 @@ namespace PetukhovaTask1_2
             Side2 = GetDistance(x3, y3, x2, y2);
             Side3 = GetDistance(x3, y3, x4, y4);
             Side4 = GetDistance(x4, y4, x1, y1);
+        }
+
+        /// <summary>
+        /// Расчет периметра четырёхугольника.
+        /// </summary>
+        /// <returns>Периметр четырёхугольника.</returns>
+        public override double Perimeter()
+        {
+            return Side1 + Side2 + Side3+ Side4;
+        }
+
+        /// <summary>
+        /// Расчет площади четырёхугольника.
+        /// </summary>
+        /// <returns>Площадь четырёхугольника.</returns>
+        public override double Area()
+        {
+            double p = this.Perimeter() / 2;
+
+            return Math.Sqrt((p - Side1) * (p - Side2) * (p - Side3) * (p - Side4));
+        }
+
+        /// <summary>
+        /// Получает строковое представление четырёхугольника.
+        /// </summary>
+        /// <returns>Строковое представление четырёхугольника.</returns>
+        public override string ToString()
+        {
+            return $"{this.Name} - ({this.Side1}, {this.Side2}, {this.Side3}, {this.Side4})";
+        }
+
+        /// <summary>
+        /// Если передаваемый объект типа Четырёхугольник,
+        /// то сравниваются их стороны. В случае, если они равны, то равны и фигуры.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Если четырёхугольники равны - true,
+        /// во всех остальных случаях - false</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Quadrangle quadrangle2)
+            {
+                if (this.Side1 == quadrangle2.Side1
+                    && this.Side2 == quadrangle2.Side2
+                    && this.Side3 == quadrangle2.Side3
+                    && this.Side3 == quadrangle2.Side4)
+                    return true;
+            }
+            else return false;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Возвращает некоторое числовое значение,
+        /// соответствующее площади данного объекта.
+        /// </summary>
+        /// <returns>Хэш-код для значения площади фигуры.</returns>
+        public override int GetHashCode()
+        {
+            return this.Area().GetHashCode();
         }
     }
 }

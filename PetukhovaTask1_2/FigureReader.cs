7e91b321efb3d72ee0f0f097cc5bbb7832451f01
Екатерина Петухова <katedzen@icloud.com>
2020-07-21 09:04:01 +0300

    work

--- PetukhovaTask1_2/FigureReader.cs
+++ PetukhovaTask1_2/FigureReader.cs
@@ -56,20 +56,18 @@ namespace PetukhovaTask1_2
 
                     if (sFigure[1].Contains("("))
                     {
-                        double[] points = new double[6];
-                        int j;
+                        double[] x = new double[3];
+                        double[] y = new double[3];
+
                         for (i = 1, j = i - 1; i < sFigure.Length; i++)
                         {                            
                             sFigure[i].Trim(new char[] { '(', ')' });
                             string[] point = sFigure[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                            if (Double.TryParse(point[0], out points[j]) && Double.TryParse(point[1], out points[++j]))
-                            {
-                                j++;
+                            if (Double.TryParse(point[0], out x[i - 1]) && Double.TryParse(point[1], out y[i - 1]))
                                 continue;
-                            }                                
                         }
 
-                        figures[i] = (FigureBase)new Triangle(name, points[0], points[1], points[2], points[3], points[4], points[5]);
+                        figures[i] = (FigureBase)new Triangle(name, x[0], y[0], x[1], y[1], x[2], y[2]);
                     }
                     else if (Double.TryParse(sFigure[1], out double side1) && Double.TryParse(sFigure[2], out double side2) && Double.TryParse(sFigure[3], out double side3))
                         figures[i] = (FigureBase)new Triangle(name, side1, side2, side3);
@@ -81,16 +79,15 @@ namespace PetukhovaTask1_2
 
                     if (sFigure[1].Contains("("))
                     {
-                        double[] points = new double[8];
+                        double[] x = new double[4];
+                        double[] y = new double[4];
+
                         for (i = 1, j = i - 1; i < sFigure.Length; i++)
                         {
                             sFigure[i].Trim(new char[] { '(', ')' });
                             string[] point = sFigure[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                            if (Double.TryParse(point[0], out points[j]) && Double.TryParse(point[1], out points[++j]))
-                            {
-                                j++;
+                            if (Double.TryParse(point[0], out x[i - 1]) && Double.TryParse(point[1], out y[i - 1]))
                                 continue;
-                            }                                
                         }
 
                         figures[i] = (FigureBase)new Quadrangle(name, points[0], points[1], points[2], points[3], points[4], points[5], points[6], points[7]);
